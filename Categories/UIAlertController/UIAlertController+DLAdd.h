d31672d560b9df4c033de5cd48f68253edf4f98d
mazengyi <semazengyi@gmail.com>
2017-06-01 10:20:09 +0800

    UIAlertController

--- Categories/UIAlertController/UIAlertController+DLAdd.h
+++ Categories/UIAlertController/UIAlertController+DLAdd.h
@@ -12,8 +12,17 @@
 
 + (instancetype)dl_alertWithTitle:(NSString *)title message:(NSString *)message;
 
++ (instancetype)dl_alertWithTitle:(NSString *)title message:(NSString *)message actionTitle:(NSString *)actionTitle;
+
++ (instancetype)dl_alertWithTitle:(NSString *)title message:(NSString *)message actionTitle:(NSString *)actionTitle handler:(void (^)(UIAlertAction *action))handler cancelTitle:(NSString *)cancelTitle;
+
+
+
 - (void)dl_addActionWithTitle:(NSString *)title handler:(void (^)(UIAlertAction *action))handler;
 - (void)dl_addCancelActionWithTitle:(NSString *)title handler:(void (^)(UIAlertAction *action))handler;
 - (void)dl_addDestructiveActionWithTitle:(NSString *)title handler:(void (^)(UIAlertAction *action))handler;
 - (void)dl_showWithViewController:(UIViewController *)viewController;
+
+
+
 @end
--- Categories/UIAlertController/UIAlertController+DLAdd.m
+++ Categories/UIAlertController/UIAlertController+DLAdd.m
@@ -16,6 +16,21 @@
     return [self alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
 }
 
++ (instancetype)dl_alertWithTitle:(NSString *)title message:(NSString *)message actionTitle:(NSString *)actionTitle
+{
+    UIAlertController *alert = [self alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:actionTitle style:UIAlertActionStyleDefault handler:nil]];
+    return alert;
+}
+
++ (instancetype)dl_alertWithTitle:(NSString *)title message:(NSString *)message actionTitle:(NSString *)actionTitle handler:(void (^)(UIAlertAction *))handler cancelTitle:(NSString *)cancelTitle
+{
+    UIAlertController *alert = [self alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:actionTitle style:UIAlertActionStyleDefault handler:handler]];
+    [alert addAction:[UIAlertAction actionWithTitle:cancelTitle style:UIAlertActionStyleCancel handler:handler]];
+    return alert;
+}
+
 
 - (void)dl_addActionWithTitle:(NSString *)title handler:(void (^)(UIAlertAction *))handler
 {
