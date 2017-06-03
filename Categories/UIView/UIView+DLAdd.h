3d08344d4ecf651d7c7035d56099644f1d24cba6
mazengyi <semazengyi@gmail.com>
2017-06-03 23:11:08 +0800

    update UIView

--- Categories/UIView/UIView+DLAdd.h
+++ Categories/UIView/UIView+DLAdd.h
@@ -9,6 +9,19 @@
 #import <UIKit/UIKit.h>
 
 @interface UIView (DLAdd)
-
 + (instancetype)dl_xibView;
+
+
+@property (nonatomic) CGFloat left;
+@property (nonatomic) CGFloat top;
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat bottom;
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+@property (nonatomic) CGFloat centerX;
+@property (nonatomic) CGFloat centerY;
+@property (nonatomic) CGPoint origin;
+@property (nonatomic) CGSize  size;     
+
+
 @end
