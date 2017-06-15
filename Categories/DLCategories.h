19ccc0d17f925cd0c1f069ffce071281203a836b
mazengyi <semazengyi@gmail.com>
2017-06-15 11:22:37 +0800

    add header

---
+++ Categories/DLCategories.h
@@ -0,0 +1,19 @@
+//
+//  DLCategories.h
+//  Pods
+//
+//  Created by famulei on 15/06/2017.
+//
+//
+
+#ifndef DLCategories_h
+#define DLCategories_h
+
+#import "NSBundle+DLAdd.h"
+#import "NSString+DLAdd.h"
+#import "NSString+DLQRCode.h"
+#import "UIAlertController+DLAdd.h"
+#import "UIApplication+DLAdd.h"
+#import "UIButton+DLAdd.h"
+
+#endif /* DLCategories_h */
