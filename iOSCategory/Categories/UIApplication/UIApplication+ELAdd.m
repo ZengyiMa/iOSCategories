710df554d6596c32a1857f8f5a7a2da3bfcb6499
mazengyi <semazengyi@gmail.com>
2017-07-11 23:41:38 +0800

    add path

--- iOSCategory/Categories/UIApplication/UIApplication+ELAdd.m
+++ iOSCategory/Categories/UIApplication/UIApplication+ELAdd.m
@@ -21,4 +21,37 @@
 - (NSString *)el_appBuildVersion {
     return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"];
 }
+
+
+- (NSURL *)el_documentsURL {
+    return [[[NSFileManager defaultManager]
+             URLsForDirectory:NSDocumentDirectory
+             inDomains:NSUserDomainMask] lastObject];
+}
+
+- (NSURL *)el_libraryURL {
+    return [[[NSFileManager defaultManager]
+             URLsForDirectory:NSLibraryDirectory
+             inDomains:NSUserDomainMask] lastObject];
+}
+
+- (NSURL *)el_cachesURL {
+    return [[[NSFileManager defaultManager]
+             URLsForDirectory:NSCachesDirectory
+             inDomains:NSUserDomainMask] lastObject];
+}
+
+- (NSString *)el_libraryPath {
+    return [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES) firstObject];
+}
+
+- (NSString *)el_cachesPath {
+    return [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+}
+
+- (NSString *)el_documentsPath {
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+}
+
+
 @end
